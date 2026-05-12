79c0deb264b2a315f01aa069084bf06a6a0d14b5
Pablo David Gonzalez Peñate <p.gonzalez@geniuskg.local>
2026-05-11 21:49:03 -0600

    Este es primer commit pablo g

---
+++ src/prueba_repositorio/main.java
@@ -0,0 +1,20 @@
+package prueba_repositorio;
+
+public class main {
+	public main(String[] args) {
+
+        int numero1 = 10;
+        int numero2 = 5;
+
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        int division = numero1 / numero2;
+
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación: " + multiplicacion);
+        System.out.println("División: " + division);
+}
+}
+
