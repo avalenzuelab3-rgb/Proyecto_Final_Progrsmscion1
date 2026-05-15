716385df9b6c7efe2f0e849a50449c95ac5ff08c
Eddy Daniel Guerra Melgar <eguerram9@miumg.edu.gt>
2026-05-14 22:33:18 -0600

    Fix clase Loan 0.01

--- src/domain/Loan.java
+++ src/domain/Loan.java
@@ -1,5 +1,13 @@
 package domain;
 
 public class Loan {
+	
+    private Material material;
+    private User user;
 
+    public Loan(Material material, User user) {
+        this.material = material;
+        this.user = user;
+    }
+    //Esta clase que nos dejara ver que usuario pidio y que material fue prestado 
 }
